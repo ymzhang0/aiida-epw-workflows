60f9cdb1b86b3088ccd4b55f2ea127d63f71aac5
Marnik Bercx <mbercx@gmail.com>
2025-03-13 15:03:15 +0100

    🔧 Rename source folder to `aiida_supercon`

--- aiida_qe_super/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-aiida_qe_super
-
-AiiDA plugin with work chains for superconductivity research.
-"""
-
-__version__ = "0.1.0a0"
---
+++ aiida_supercon/__init__.py
@@ -0,0 +1,5 @@
+"""
+`aiida-supercon`: AiiDA plugin package with work chains for superconductivity research.
+"""
+
+__version__ = "0.1.0"
